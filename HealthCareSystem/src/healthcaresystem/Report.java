5c80f29a2a5f5b46d6be1f02f661c0e8a9987a7c
Ahmadbinshoaib <ahmadshoaib1044@gmail.com>
2021-06-19 23:10:06 +0500

    LPP

--- HealthCareSystem/src/healthcaresystem/Report.java
+++ HealthCareSystem/src/healthcaresystem/Report.java
@@ -7,6 +7,20 @@ package healthcaresystem;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import static java.awt.print.Printable.NO_SUCH_PAGE;
+import static java.awt.print.Printable.PAGE_EXISTS;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,6 +28,7 @@ import javax.swing.table.DefaultTableModel;
  */
 public class Report extends javax.swing.JFrame {
     HealthCareSystem desk= HealthCareSystem.getInstance();
+    Double bHeight=0.0;
     
     //DeskPerson
     String header[] = new String[] {"ID", "Name", "Email", "Phone Number", "CNIC","Age", "Gender", "Martial", "Salray", "Date", "Blood Group", "Address", "Username", "Password"};
@@ -4023,6 +4038,19 @@ public class Report extends javax.swing.JFrame {
 
     private void jLabel39MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel39MouseClicked
         // TODO add your handling code here:
+        bHeight = Double.valueOf(desk.desklist.size());
+        //JOptionPane.showMessageDialog(rootPane, bHeight);
+        
+        PrinterJob pj = PrinterJob.getPrinterJob();        
+        pj.setPrintable(new BillPrintable(),getPageFormat(pj));
+        try {
+             pj.print();
+          
+        }
+         catch (PrinterException ex) {
+                 ex.printStackTrace();
+        }
+        
     }//GEN-LAST:event_jLabel39MouseClicked
 
     private void jTextField18ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField18ActionPerformed
@@ -4916,7 +4944,107 @@ public class Report extends javax.swing.JFrame {
     private void jTextField87ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField87ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField87ActionPerformed
+public PageFormat getPageFormat(PrinterJob pj)
+{
+    
+    PageFormat pf = pj.defaultPage();
+    Paper paper = pf.getPaper();    
 
+    double bodyHeight = bHeight;  
+    double headerHeight = 5.0;                  
+    double footerHeight = 5.0;        
+    double width = cm_to_pp(8); 
+    double height = cm_to_pp(headerHeight+bodyHeight+footerHeight); 
+    paper.setSize(width, height);
+    paper.setImageableArea(0,10,width,height - cm_to_pp(1));  
+            
+    pf.setOrientation(PageFormat.PORTRAIT);  
+    pf.setPaper(paper);    
+
+    return pf;
+}
+ protected static double cm_to_pp(double cm)
+    {            
+	        return toPPI(cm * 0.393600787);            
+    }
+ 
+protected static double toPPI(double inch)
+    {            
+	        return inch * 72d;            
+    }
+public class BillPrintable implements Printable {
+    
+   
+    
+    
+  public int print(Graphics graphics, PageFormat pageFormat,int pageIndex) 
+  throws PrinterException 
+  {    
+      
+      int r= desk.desklist.size();
+      ImageIcon icon=new ImageIcon("C:UsersccsDocumentsNetBeansProjectsvideo TestPOSInvoicesrcposinvoicemylogo.jpg"); 
+      int result = NO_SUCH_PAGE;    
+        if (pageIndex == 0) {                    
+        
+            Graphics2D g2d = (Graphics2D) graphics;                    
+            double width = pageFormat.getImageableWidth();                               
+            g2d.translate((int) pageFormat.getImageableX(),(int) pageFormat.getImageableY()); 
+
+
+
+          //  FontMetrics metrics=g2d.getFontMetrics(new Font("Arial",Font.BOLD,7));
+        
+        try{
+            int y=20;
+            int yShift = 10;
+            int headerRectHeight=15;
+           // int headerRectHeighta=40;
+            
+                
+            g2d.setFont(new Font("Monospaced",Font.PLAIN,9));
+            g2d.drawImage(icon.getImage(), 50, 20, 90, 30, rootPane);y+=yShift+30;
+            g2d.drawString("-------------------------------------",12,y);y+=yShift;
+            g2d.drawString("         CodeGuid.com        ",12,y);y+=yShift;
+            g2d.drawString("   No 00000 Address Line One ",12,y);y+=yShift;
+            g2d.drawString("   Address Line 02 SRI LANKA ",12,y);y+=yShift;
+            g2d.drawString("   www.facebook.com/CodeGuid ",12,y);y+=yShift;
+            g2d.drawString("        +94700000000      ",12,y);y+=yShift;
+            g2d.drawString("-------------------------------------",12,y);y+=headerRectHeight;
+
+            g2d.drawString(" Item Name                  Price   ",10,y);y+=yShift;
+            g2d.drawString("-------------------------------------",10,y);y+=headerRectHeight;
+     
+            for(int s=0; s<r; s++)
+            {
+            g2d.drawString(" "+desk.desklist.get(s).getName()+"                            ",10,y);y+=yShift;
+            g2d.drawString("      "+desk.desklist.get(s).getName()+" * "+desk.desklist.get(s).getName(),10,y); g2d.drawString(desk.desklist.get(s).getName(),160,y);y+=yShift;
+
+            }
+          
+            g2d.drawString("-------------------------------------",10,y);y+=yShift;
+            g2d.drawString(" Total amount:               "+desk.desklist.get(0).getName()+"   ",10,y);y+=yShift;
+            g2d.drawString("-------------------------------------",10,y);y+=yShift;
+            g2d.drawString(" Cash      :                 "+desk.desklist.get(0).getName()+"   ",10,y);y+=yShift;
+            g2d.drawString("-------------------------------------",10,y);y+=yShift;
+            g2d.drawString(" Balance   :                 "+desk.desklist.get(0).getName()+"   ",10,y);y+=yShift;
+  
+            g2d.drawString("*************************************",10,y);y+=yShift;
+            g2d.drawString("       THANK YOU COME AGAIN            ",10,y);y+=yShift;
+            g2d.drawString("*************************************",10,y);y+=yShift;
+            g2d.drawString("       SOFTWARE BY:CODEGUID          ",10,y);y+=yShift;
+            g2d.drawString("   CONTACT: contact@codeguid.com       ",10,y);y+=yShift;       
+           
+
+    }
+    catch(Exception e){
+    e.printStackTrace();
+    }
+
+              result = PAGE_EXISTS;    
+          }    
+          return result;    
+      }
+   }
     /**
      * @param args the command line arguments
      */
